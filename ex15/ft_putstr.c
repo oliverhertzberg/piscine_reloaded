d420dd7afcbbf9fcd8435d9445c06d9c2faea663
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2023-10-01 18:11:16 +0300

    do ex15 putstr

---
+++ ex15/ft_putstr.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+int	main(void)
+{
+	ft_putstr("Hello World! 1231313131 testing testing");
+	return(0);
+}
