18d1c99c22affe9874c4c1b42ea429d408eb4d90
Oliver Hertzberg <oliverhertzberg@hotmail.com>
2023-10-01 16:51:03 +0300

    do ex06 print_alphabet

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void);
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+	while(letter <= 'z')
+	{
+		ft_putchar(letter++);
+	}
+}
+
+int	main(void)
+  {
+ 	ft_print_alphabet();
+ 	return (0);
+  }
+ 
